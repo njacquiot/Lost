9481115b45ba90f0b4e05347318005c32d1769b3
njacquiot <nicojacq28@orange.fr>
2019-02-28 17:09:11 +0100

    Lancement serveur

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var socket = require('socket.io');
+
+//App setup
+var app = express();
+var server = app.listen(4000, function(){
+  console.log('Listenning to request on port 4000');
+});
+
+
+//Static files
+app.use(express.static('public'));
+
+
+//Socket setup
+var io = socket(server);
+
+io.on('connection', function(socket){
+  console.log('made socket connection', socket.id) // Unique id par socket
+
+});
